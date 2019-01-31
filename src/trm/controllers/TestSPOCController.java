97fc8358cdfbe9edbfcb457057ed9b3b4685809f
Daniel Albornoz <dalbornoz1996@gmail.com>
2019-01-31 16:25:18 -0700

    added TestSPOCController.java controller and beginning of SPOCForm.jsp view

---
+++ src/trm/controllers/TestSPOCController.java
@@ -0,0 +1,30 @@
+package trm.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class TestSPOCController {
+	//Initial Page (root directory)
+	@RequestMapping(value="/")
+	public String test()
+	{
+		return "SPOCForm";
+	}	
+	//function that will take data for SPOC and store it into object with DAO
+	//currently simply returns the same view
+	@RequestMapping(value="SPOCInputData")
+	public String spocInputData()
+	{
+		//insert into dataBase
+		//int ret = new CustomerCRUDService().updateCustomer(customer.getCustId(), customer.getCustCity(), customer.getCustPhone());
+		
+		return "SPOCForm";
+	}
+	
+}
