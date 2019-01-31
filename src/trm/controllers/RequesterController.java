fe5b47291b1fbb0f3461992715c073a8c2ffb0d0
rie-kumar <rie.kumar2@gmail.com>
2019-01-31 16:03:32 -0700

    Added confirm schedule and delete request routes

--- src/trm/controllers/RequesterController.java
+++ src/trm/controllers/RequesterController.java
@@ -1,4 +1,4 @@
-package trm.controller;
+package trm.controllers;
 
 import java.sql.Timestamp;
 
@@ -7,6 +7,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RequesterController {
@@ -49,6 +50,41 @@ public class RequesterController {
 			return "mainview";
 //		else
 //			return "error";
+	}
+	
+	@RequestMapping(value="requester/requests/{id}", method=RequestMethod.DELETE)
+	public String deleteRequest(@PathVariable("id") int reqId)
+	{
+		int ret = new RequestCRUDService().deleteRequest(reqId);
+		
+		if(ret>0)
+			return "redirect:/requester/dashboard";
+		else
+			return "error";
+	}
+	
+	@RequestMapping(value="requester/schedules/{id}/confirm", method=RequestMethod.PUT)
+	public String confirmSchedule(@PathVariable("id") int scheduleId, ModelMap map)
+	{
+		int ret = new RequestCRUDService().confirmSchedule(scheduleId);
+		
+		if (ret>0)
+			return "redirect:/requester/dashboard";
+		else
+			return "error";
+	}
+	
+	class RequestCRUDService
+	{
+		public int confirmSchedule(int id)
+		{
+			return 0;
+		}
+		
+		public int deleteRequest(int id)
+		{
+			return 0;
+		}
 	}
 }
 
