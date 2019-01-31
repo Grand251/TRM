0f65969ec80e00264c05258ae3e1cd24250c01bf
Huey <yeuhpadua@gmail.com>
2019-01-31 16:11:15 -0700

    first commit

--- src/trm/controllers/SpocController.java
+++ src/trm/controllers/SpocController.java
@@ -33,5 +33,48 @@ public class SpocController {
 		
 		return "allrequests";
 	}
+	// Controllers for ClassroomTrainingMode
+	@RequestMapping(value="CRTMode")
+	public String SelectedCRTMode(ModelMap map)
+	{
+		//Get updated internal_training_request and save to temp
+		//map temp to command to be passed 
+		
+		return "CRTModeForm";
+	}
+	
+	@RequestMapping(value="ConfirmCRTMode")
+	public String ConfirmCRTMode(ModelMap map)
+	{
+		//Save updated request model from spring form
+		//-location, room booked
+		//call service to update internal_training_req to database
+		//if updated, redirect to viewAllRequests
+		//else go to error
+		
+		return "";
+	}
+	
+	//Controllers for WebTrainingMode
+	@RequestMapping(value="WTMode")
+	public String SelectedWTMode(ModelMap map)
+	{
+		//Get updated internal_training_request and save to temp
+		//map temp to command to be passed 
+		
+		return "WTModeForm";
+	}
+	
+	@RequestMapping(value="ConfirmWTMode")
+	public String ConfirmWTMode(ModelMap map)
+	{
+		//Save updated request model from spring form
+		//-web services chosen
+		//call service to update internal_training_req to database
+		//if updated, redirect to viewAllRequests
+		//else go to error
+		
+		return "";
+	}
 	
 }
