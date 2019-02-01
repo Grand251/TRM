2e3779760fed8bd31b3d3bc73ea9affe6b696de6
Kei <Kei_Ng@syntelinc.com>
2019-02-01 14:21:21 -0700

    internaltrainer and internaltrainingrequest initial commit

---
+++ src/trm/dao/internaltrainer/InternalTrainer.java
@@ -0,0 +1,43 @@
+package dao.internaltrainer;
+
+
+import trm.employee.Employee;
+
+/*
+ * CREATE TABLE INTERNAL_TRAINER
+(
+  it_trainer_id number(7) references employee(employee_id) unique not null,
+  profile_file_path varchar(100),
+  trainer_location varchar(20)
+);
+ * 
+ */
+
+public class InternalTrainer {
+	private Employee trainer;
+	private String profileFilePath;
+	private String trainerLocation;
+	
+	public void displayDetails() {
+		System.out.println(trainer.getEmployee_id() + " " + getProfileFilePath() + " " + getTrainerLocation());
+	}
+	
+	public Employee getTrainer() {
+		return trainer;
+	}
+	public void setTrainer(Employee trainer) {
+		this.trainer = trainer;
+	}
+	public String getProfileFilePath() {
+		return profileFilePath;
+	}
+	public void setProfileFilePath(String profileFilePath) {
+		this.profileFilePath = profileFilePath;
+	}
+	public String getTrainerLocation() {
+		return trainerLocation;
+	}
+	public void setTrainerLocation(String trainerLocation) {
+		this.trainerLocation = trainerLocation;
+	}
+}
---
+++ src/trm/dao/internaltrainer/InternalTrainerMapper.java
@@ -0,0 +1,24 @@
+package dao.internaltrainer;
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import trm.employee.Employee;
+import trm.employee.EmployeeCRUDService;
+
+public class InternalTrainerMapper implements RowMapper<InternalTrainer>{
+
+	@Override
+	public InternalTrainer mapRow(ResultSet rs, int rowNum) throws SQLException {
+		InternalTrainer it = new InternalTrainer();
+		Employee trainer = new EmployeeCRUDService().getEmployeeById(rs.getInt(1));
+		it.setTrainer(trainer);
+		it.setProfileFilePath(rs.getString(2));
+		it.setTrainerLocation(rs.getString(3));
+		return it;
+	}
+
+}
