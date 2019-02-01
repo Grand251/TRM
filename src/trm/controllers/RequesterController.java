287a80748a33278eba42a349c45f0b17796953fe
eliasgeorge53 <eliasgeorge53@gmail.com>
2019-02-01 11:07:05 -0700

    --

--- src/trm/controllers/RequesterController.java
+++ src/trm/controllers/RequesterController.java
@@ -11,85 +11,89 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RequesterController {
-
-
+	
 	@RequestMapping(value = "/")
-	public String openMainView() {
-		return "mainview";
+	public String loginView(ModelMap map) {
+			return "allrequests";//Temp return value for testing
+			//return "loginpage"
+	}
+
+
+	@RequestMapping(value = "openmain")
+	public String openMainView(@ModelAttribute("usertype") int userType, ModelMap map) {
+		if (userType == 0)
+			return "requestermain";
+		if (userType == 1)
+			return "SPOCmain";
+		else
+			return "execmain";
 	}
 
 	@RequestMapping(value = "createrequest")
-	public String createNewRequests(ModelMap map) 
-	{
+	public String createNewRequests(ModelMap map) {
 		return "newrequestform";
 	}
-	
-	@RequestMapping( value = "addnewrequest")
-	public String addNewRequest(@ModelAttribute("request") Request request, ModelMap map) 
-	{
+
+	@RequestMapping(value = "addnewrequest")
+	public String addNewRequest(@ModelAttribute("request") Request request, ModelMap map) {
 		Timestamp requestTime = new Timestamp(System.currentTimeMillis());
 		request.setRequestTimeStamp(requestTime);
 //		Boolean bool = PMServices.addRequest(request)
 //		if(bool)
-			return "mainview";
+		return "mainview";
 //		else
 //			return "error";
 	}
-	
-	@RequestMapping(value = "editrequest/{requestId}")
+
+	@RequestMapping(value = "editrequest")
 	public String editCustomer(@PathVariable("requestId") int reqID, ModelMap map) {
 //		Request request = new TRM.DAO.TrainingRequest().getRequestById(reqID);
 //		map.addAttribute("command",request);
 		return "editform";
 	}
-	
+
 	@RequestMapping(value = "saveUpdateData")
 	public String saveUpdatedCustomerDetails(@ModelAttribute("request") Request request, ModelMap map) {
 //		boolean bool = new PMServices.updateRequest(request);
 //		if (bool)
-			return "mainview";
+		return "mainview";
 //		else
 //			return "error";
 	}
-	
-	@RequestMapping(value="requester/requests/{id}", method=RequestMethod.DELETE)
-	public String deleteRequest(@PathVariable("id") int reqId)
-	{
+
+	@RequestMapping(value = "requester/requests/{id}", method = RequestMethod.DELETE)
+	public String deleteRequest(@PathVariable("id") int reqId) {
 		int ret = new RequestCRUDService().deleteRequest(reqId);
-		
-		if(ret>0)
+
+		if (ret > 0)
 			return "redirect:/requester/dashboard";
 		else
 			return "error";
 	}
-	
-	@RequestMapping(value="requester/schedules/{id}/confirm", method=RequestMethod.PUT)
-	public String confirmSchedule(@PathVariable("id") int scheduleId, ModelMap map)
-	{
+
+	@RequestMapping(value = "requester/schedules/{id}/confirm", method = RequestMethod.PUT)
+	public String confirmSchedule(@PathVariable("id") int scheduleId, ModelMap map) {
 		int ret = new RequestCRUDService().confirmSchedule(scheduleId);
-		
-		if (ret>0)
+
+		if (ret > 0)
 			return "redirect:/requester/dashboard";
 		else
 			return "error";
 	}
-	
-	class RequestCRUDService
-	{
-		public int confirmSchedule(int id)
-		{
+
+	class RequestCRUDService {
+		public int confirmSchedule(int id) {
 			return 0;
 		}
-		
-		public int deleteRequest(int id)
-		{
+
+		public int deleteRequest(int id) {
 			return 0;
 		}
 	}
 }
 
 //dummy class
-class Request{
+class Request {
 	private Timestamp requestTimeStamp;
 
 	public Timestamp getRequestTimeStamp() {
--- src/trm/controllers/TestSPOCController.java
+++ src/trm/controllers/TestSPOCController.java
@@ -10,21 +10,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestSPOCController {
-	//Initial Page (root directory)
-	@RequestMapping(value="/")
-	public String test()
-	{
-		return "SPOCForm";
-	}	
-	//function that will take data for SPOC and store it into object with DAO
-	//currently simply returns the same view
-	@RequestMapping(value="SPOCInputData")
-	public String spocInputData()
-	{
-		//insert into dataBase
-		//int ret = new CustomerCRUDService().updateCustomer(customer.getCustId(), customer.getCustCity(), customer.getCustPhone());
-		
-		return "SPOCForm";
-	}
+//	//Initial Page (root directory)
+//	@RequestMapping(value="/")
+//	public String test()
+//	{
+//		return "SPOCForm";
+//	}	
+//	//function that will take data for SPOC and store it into object with DAO
+//	//currently simply returns the same view
+//	@RequestMapping(value="SPOCInputData")
+//	public String spocInputData()
+//	{
+//		//insert into dataBase
+//		//int ret = new CustomerCRUDService().updateCustomer(customer.getCustId(), customer.getCustCity(), customer.getCustPhone());
+//		
+//		return "SPOCForm";
+//	}
 	
 }
