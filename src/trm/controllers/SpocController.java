514cbb3448c3c22c2b7b2de1dc3e47e93d4dcd25
Daniel Albornoz <dalbornoz1996@gmail.com>
2019-02-04 03:01:31 -0700

    implemented functionality to view all employees, schedules, training requests, and internal training requests in the database; implemented functionality to insert an internal training request; added simple error page

--- src/trm/controllers/SpocController.java
+++ src/trm/controllers/SpocController.java
@@ -1,18 +1,123 @@
 package trm.controllers;
 
 
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.ServletRequestDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+import trm.dao.employee.Employee;
+import trm.dao.employee.EmployeeCRUDService;
 import trm.dao.internaltrainingrequest.InternalTrainingCRUD;
-import trm.dao.internaltrainingrequest.*;
-import trm.dao.trainingrequest.*;
+import trm.dao.internaltrainingrequest.InternalTrainingRequest;
+import trm.dao.trainingrequest.TrainingRequest;
+import trm.dao.trainingrequest.TrainingRequestCRUD;
+import trm.dao.trainingschedule.TrainingSchedule;
+import trm.dao.trainingschedule.TrainingScheduleCRUDService;
+
+import trm.editors.EmployeeEditor;
+import trm.editors.TrainingRequestEditor;
+import trm.editors.TrainingScheduleEditor;
+
 
 @Controller
 public class SpocController {
-	
+	/*
+	@RequestMapping(value="showoneTR/{employee_id}")
+	public String showOneTR(@ModelAttribute("trainingRequest") int trainer_id, ModelMap map)
+	{
+		InternalTrainer internalTrainer = new InternalTrainerCRUD().getInternalTrainerById(trainer_id);
+		map.addAttribute("trainingRequest", internalTrainer);
+		return "trbyid";
+	}*/	
+	//initBinder allows Spring forms to map user input to attributes of type Employee, TrainingRequest, and TrainingSchedule
+	@InitBinder
+	public void initBinder(HttpServletRequest request, ServletRequestDataBinder binder ) {
+
+	        binder.registerCustomEditor(Employee.class, new EmployeeEditor(new EmployeeCRUDService()));
+	        binder.registerCustomEditor(TrainingRequest.class, new TrainingRequestEditor(new TrainingRequestCRUD()));
+	        binder.registerCustomEditor(TrainingSchedule.class, new TrainingScheduleEditor(new TrainingScheduleCRUDService()));
+	  
+	} 
+	  
+	@RequestMapping(value="showallitr")
+	public String showallITRequests(ModelMap map) {
+		List<InternalTrainingRequest> internalTrainingRequests = new InternalTrainingCRUD().getAllItr();
+		map.addAttribute("internalTrainingRequests", internalTrainingRequests);
+		
+		return "allitrequests";
+	}
+	@RequestMapping(value="showallemployees")
+	public String showallEmployees(ModelMap map) {
+		List<Employee> employees = new EmployeeCRUDService().getAllEmployee();
+		map.addAttribute("employees", employees);
+		
+		return "allemployees";
+	}
+	@RequestMapping(value="showalltr")
+	public String showallTRequests(ModelMap map) {
+		List<TrainingRequest> trainingRequests = new TrainingRequestCRUD().getAllTrainingRequest();
+		map.addAttribute("trainingRequests", trainingRequests);
+		
+		return "alltrequests";
+	}
+	@RequestMapping(value="showallschedules")
+	public String showallSchedules(ModelMap map) {
+		List<TrainingSchedule> trainingSchedules = new TrainingScheduleCRUDService().getAllTrainingSchedule();
+		map.addAttribute("trainingSchedules", trainingSchedules);
+		
+		return "allschedules";
+	}
+	@RequestMapping(value="insertitrform")
+	public String insertITRequestForm(ModelMap map)
+	{
+		InternalTrainingRequest internalTrainingRequest = new InternalTrainingRequest();
+		map.addAttribute("command", internalTrainingRequest);
+
+		List<Employee> employees = new EmployeeCRUDService().getAllEmployee();
+		List<TrainingRequest> trainingRequests = new TrainingRequestCRUD().getAllTrainingRequest();
+		List<TrainingSchedule> schedules = new TrainingScheduleCRUDService().getAllTrainingSchedule();
+		
+
+		map.addAttribute("employees", employees);
+		map.addAttribute("trainingRequests", trainingRequests);
+		map.addAttribute("schedules", schedules);
+
+		
+		
+		return "insertitr";
+	}
+	@RequestMapping(value="newitr")
+	public String insertITRequest(@ModelAttribute("internalTrainingRequest") InternalTrainingRequest internalTrainingRequest)
+	{
+		
+		System.out.println(internalTrainingRequest.getItrTrainer());
+		internalTrainingRequest.setItrId(99999);
+		int ret = new InternalTrainingCRUD().insertItr(internalTrainingRequest);
+		
+		if(ret > 0)
+			return "redirect:/showallitr";
+		else
+			return "error";
+	}
+	@RequestMapping(value="DT")
+	public String showDTRequests(ModelMap model) {
+		
+		return "dtform";
+	}
+	@RequestMapping(value="VT")
+	public String showVTRequests(ModelMap model) {
+		
+		return "vtform";
+	}
 	@RequestMapping(value="pmastatus")
 	public String showPMAStatus(ModelMap model) {
 		
@@ -31,55 +136,56 @@ public class SpocController {
 		return "requestoverview";
 	}
 	
-	@RequestMapping(value="spocdashboard")
-	public String showAllRequests(ModelMap model) {
+	@RequestMapping(value="allrequests")
+	public String showAllRequests(ModelMap map) {
 		
-		return "spocdashboard";
+		List<TrainingRequest> trainingRequests = new TrainingRequestCRUD().getAllTrainingRequest();
+		map.addAttribute("trainingRequests", trainingRequests);
+		return "allrequests";
 	}
 	// Controllers for ClassroomTrainingMode
-	@RequestMapping(value="crtmode")
-	public String SelectedCRTMode(ModelMap map, @ModelAttribute("ITRequest") InternalTrainingRequest ITRequest)
+	@RequestMapping(value="CRTMode")
+	public String SelectedCRTMode(ModelMap map)
 	{
-		TrainingSchedule schedule = new TrainingSchedule();
-		ITRequest.setInternal_training_mode("CRT");
-		map.addAttribute("command", ITRequest);
-		map.addAttribute("command", schedule);
-		return "crtmodeform";
+		//Get updated internal_training_request and save to temp
+		//map temp to command to be passed 
+		
+		return "CRTModeForm";
 	}
 	
-	@RequestMapping(value="confirmcrtMode")
-	public String ConfirmCRTMode(ModelMap map, @ModelAttribute("schedule") TrainingSchedule schedule, 
-			@ModelAttribute("ITRequest") trm.dao.internaltrainingrequest.InternalTrainingRequest ITRequest)
+	@RequestMapping(value="ConfirmCRTMode")
+	public String ConfirmCRTMode(ModelMap map)
 	{
-		ITRequest.setItrId(schedule.getTraining_schedule_id());
-		int ret = new InternalTrainingCRUD().updateItr(ITRequest);
-		if (ret > 0)
-			return "spocdashboard";
-		else
-			return "error";
+		//Save updated request model from spring form
+		//-location, room booked
+		//call service to update internal_training_req to database
+		//if updated, redirect to viewAllRequests
+		//else go to error
+		
+		return "";
 	}
 	
 	//Controllers for WebTrainingMode
-	@RequestMapping(value="wtmode")
-	public String SelectedWTMode(ModelMap map, @ModelAttribute("ITRequest") InternalTrainingRequest ITRequest)
+	@RequestMapping(value="WTMode")
+	public String SelectedWTMode(ModelMap map)
 	{
-		TrainingSchedule schedule = new TrainingSchedule();
-		ITRequest.setInternal_training_mode("WT");
-		map.addAttribute("command", ITRequest);
-		map.addAttribute("command", schedule);
-		return "crtmodeform";
+		//Get updated internal_training_request and save to temp
+		//map temp to command to be passed 
+		
+		return "WTModeForm";
 	}
 	
-	@RequestMapping(value="confirmwtMode")
-	public String ConfirmWTMode(ModelMap map, @ModelAttribute("schedule") TrainingSchedule schedule, 
-			@ModelAttribute("ITRequest") trm.dao.internaltrainingrequest.InternalTrainingRequest ITRequest)
+	@RequestMapping(value="ConfirmWTMode")
+	public String ConfirmWTMode(ModelMap map)
 	{
-		ITRequest.setItrId(schedule.getTraining_schedule_id());
-		int ret = new InternalTrainingCRUD().updateItr(ITRequest);
-		if (ret > 0)
-			return "spocdashboard";
-		else
-			return "error";
+		//Save updated request model from spring form
+		//-web services chosen
+		//call service to update internal_training_req to database
+		//if updated, redirect to viewAllRequests
+		//else go to error
+		
+		return "";
 	}
 	
 }
+
---
+++ views/allemployees.jsp
@@ -0,0 +1,51 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<%@ taglib prefix="c"  uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<h1> All Employees</h1>
+<br>
+	<table border="2">
+		<tr>
+	    	<th id="employee_id">ID</th>
+	    	<th id="last_name">Last Name</th>
+	    	<th id="first_name">First Name</th>
+	    	<th id="user_name">Username</th>
+	    	<th id="phone_number">Phone Number</th>
+	    	<th id="email">E-mail Address</th>
+	    	<th id="street">Street</th>
+	    	<th id="city">City</th>
+	    	<th id="state">State</th>
+	    	<th id="country">Country</th>
+	    	<th id="job_title">Job Title</th>
+	    	<th id="vertical">Vertical</th>
+	    	<th id="project">Project</th>
+	    	<th id="pid">Project ID</th>
+	    </tr>
+		<c:forEach items="${employees}" var="employee">
+			<tr>
+				<td>${employee.employee_id}</td>
+				<td>${employee.last_name}</td>
+				<td>${employee.first_name}</td>
+				<td>${employee.user_name}</td>
+				<td>${employee.phone_number}</td>
+				<td>${employee.email}</td>
+				<td>${employee.street}</td>
+				<td>${employee.city}</td>
+				<td>${employee.state}</td>
+				<td>${employee.country}</td>
+				<td>${employee.job_title}</td>
+				<td>${employee.vertical}</td>
+				<td>${employee.project}</td>
+				<td>${employee.pid}</td>
+						
+			</tr>
+		</c:forEach>
+	</table>
+</body>
+</html>
